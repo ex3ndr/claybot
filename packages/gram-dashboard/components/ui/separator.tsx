1971e337ac225fc2c0c9cedcb979cea1c19e4749
Steve Korshakov <steve@korshakov.com>
2026-01-30 20:39:25 -0800

    Fix separator ref typing

--- packages/gram-dashboard/components/ui/separator.tsx
+++ packages/gram-dashboard/components/ui/separator.tsx
@@ -5,19 +5,23 @@ type SeparatorProps = React.HTMLAttributes<HTMLDivElement> & {
   orientation?: "horizontal" | "vertical";
 };
 
-function Separator({ className, orientation = "horizontal", ...props }: SeparatorProps) {
-  return (
-    <div
-      className={cn(
-        "shrink-0 bg-border",
-        orientation === "horizontal" ? "h-px w-full" : "h-full w-px",
-        className
-      )}
-      role="separator"
-      data-orientation={orientation}
-      {...props}
-    />
-  );
-}
+const Separator = React.forwardRef<HTMLDivElement, SeparatorProps>(
+  ({ className, orientation = "horizontal", ...props }, ref) => {
+    return (
+      <div
+        ref={ref}
+        className={cn(
+          "shrink-0 bg-border",
+          orientation === "horizontal" ? "h-px w-full" : "h-full w-px",
+          className
+        )}
+        role="separator"
+        data-orientation={orientation}
+        {...props}
+      />
+    );
+  }
+);
+Separator.displayName = "Separator";
 
 export { Separator };
